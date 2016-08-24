b433cba1d0af2b8c23268bc5d3c7b267f5b72807
Hugo <hugocm93@gmail.com>
2016-08-24 14:55:07 -0300

    Files added. Lab0

--- Lab0/main.c
+++ Lab0/main.c
@@ -14,10 +14,17 @@ int main ( void )
 	matriz[2][2] = 3;
 
 	mat_imprime( 3, 3, matriz );
-	mat_libera( 3 , matriz );
-	matriz = NULL;
+	//mat_libera( 3 , matriz );
+	//matriz = NULL;
 	mat_imprime( 3, 3, matriz );
+
+	double v1[] = { 1, 2, 3 };
+	double v2[ 3 ];
 	
+	mat_multv( 3, 3, matriz, v1, v2 );
+
+	printf("%lf %lf %lf\n", v2[0], v2[1], v2[2]);
+
 	return 0;
 }
 
