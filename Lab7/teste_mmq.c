133cc5143be9bcc2ff2a8abe59718142170e628c
Hugo Costa Machado <hugocm93@gmail.com>
2016-10-24 22:04:48 -0200

    almost done

--- Lab7/teste_mmq.c
+++ Lab7/teste_mmq.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "mmq.h"
 
@@ -8,6 +9,8 @@ void teste(char c, int m, int n, double** A, double* b);
 
 int main(void)
 {
+  printf("******  Questão 3\n\n");
+
   Matriz m1 = mat_cria(5, 3);
   m1[0][0] = 3;
   m1[0][1] = -1;
@@ -73,6 +76,51 @@ int main(void)
 
   mat_libera(5, m1);
   mat_libera(5, m2);
+
+
+  /********************************/
+  printf("******  Questão 4\n\n");
+
+  double c[8] = {8, 12.3, 15.5, 16.8, 17.1, 15.8, 15.2, 14};
+
+  Matriz m3 = mat_cria(8, 2);
+  m3[0][0] = 1;
+  m3[1][0] = 1;
+  m3[2][0] = 1;
+  m3[3][0] = 1;
+  m3[4][0] = 1;
+  m3[5][0] = 1;
+  m3[6][0] = 1;
+  m3[7][0] = 1;
+  m3[0][1] = 1;
+  m3[1][1] = 2;
+  m3[2][1] = 3;
+  m3[3][1] = 4;
+  m3[4][1] = 5;
+  m3[5][1] = 6;
+  m3[6][1] = 7;
+  m3[7][1] = 8;
+
+  double b3[8];
+  b3[0] = log(c[0]) - log(1);
+  b3[1] = log(c[1]) - log(2);
+  b3[2] = log(c[2]) - log(3);
+  b3[3] = log(c[3]) - log(4);
+  b3[4] = log(c[4]) - log(5);
+  b3[5] = log(c[5]) - log(6);
+  b3[6] = log(c[6]) - log(7);
+  b3[7] = log(c[7]) - log(8);
+
+  mat_imprime("m3", 8, 2, m3);
+  vec_imprime("b3", b3, 8);
+
+  double* res3 = mmq(8, 2, m3, b3);
+  double a = exp(res3[0]);
+  double b = res3[1];
+  printf("a: %.4g b:%.4g\n", a, b);
+
+  free(res3);
+  mat_libera(8, m3);
 
   return 0;
 }
