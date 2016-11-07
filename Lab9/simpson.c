426fcde517ffe56c252e7e6c0b7c08f49b3dca62
Hugo <hugocm93@gmail.com>
2016-11-07 14:39:47 -0200

    done

--- Lab9/simpson.c
+++ Lab9/simpson.c
@@ -1,11 +1,35 @@
+#include <math.h>
+
 #include "simpson.h"
 
 double Simpson(double a, double b, double (*f) (double x), double* v)
 {
-	return 0;
+	double c = (a + b)/2;
+
+	double fa = f(a);
+	double fb = f(b);
+	double fc = f(c);
+
+	double Sab = ((b - a)/6)*(fa + 4*fc + fb);
+	double Sac = ((c - a)/6)*(fa + 4*f((a + c)/2) + fc);
+	double Scb = ((b - c)/6)*(fc + 4*f((c + b)/2) + fb);
+
+	*v = Sac + Scb;
+
+	return fabs((Sab - *v)/15);
 }
 
 double AdaptiveSimpson(double a, double b, double (*f) (double x), double tol)
 {
-	return 0;
+	double v;
+
+	if(Simpson(a, b, f, &v) <= tol)
+	{
+		return v;
+	}
+	else
+	{
+		double c = (a + b)/2;
+		return AdaptiveSimpson(a, c, f, tol/2) + AdaptiveSimpson(c, b, f, tol/2);
+	}
 }
--- Lab9/simpson_teste.c
+++ Lab9/simpson_teste.c
@@ -23,13 +23,15 @@ double func3(double x)
 
 int main(void)
 {
-	double r1 = AdaptiveSimpson(0, 1, func1, 7);
+	double tol = 0.5 * pow(10, -7);
 
-	double r2 = 1/(sqrt(2*M_PI))*AdaptiveSimpson(-1, 1, func2, 7);
+	double r1 = AdaptiveSimpson(0, 1, func1, tol);
 
-	double r3 = 2/(sqrt(M_PI))*AdaptiveSimpson(0, 3, func3, 7);
+	double r2 = 1/(sqrt(2*M_PI))*AdaptiveSimpson(-1, 1, func2, tol);
 
-	printf("func1: %f  func2: %f func3: %f\n", r1, r2, r3);
+	double r3 = 2/(sqrt(M_PI))*AdaptiveSimpson(0, 3, func3, tol);
+
+	printf("func1: %.7f  func2: %.7f func3: %.7f\n", r1, r2, r3);
 
 	return 0;
 }
