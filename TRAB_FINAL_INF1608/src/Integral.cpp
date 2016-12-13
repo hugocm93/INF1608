23af110e0d26bb5402367e4fed415ca2ab2b9a07
Hugo Machado <hugocm93@gmail.com>
2016-12-13 19:47:56 -0200

    DONE

--- TRAB_FINAL_INF1608/src/Integral.cpp
+++ TRAB_FINAL_INF1608/src/Integral.cpp
@@ -7,12 +7,11 @@ double Integral::simpson( std::function< double ( double ) >& function, double a
     double sum = 0;
     double s1 = 0, s2 = 0;
 
-    if( b == 0 )
+    if( b - a == 0 )
     {
         return 0;
     }
 
-//    assert( b > h );
     assert( b > a );
 
     int i, n = ( b - a)/h;
